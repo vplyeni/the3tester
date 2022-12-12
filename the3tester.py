3c50beed25bc558272b2ffd9838cf901942e62dc
taylananas <shnitailan65@gmail.com>
2022-12-13 00:32:21 +0300

    THE3 TESTER

---
+++ the3tester.py
@@ -0,0 +1,43 @@
+from the3 import pattern_search
+
+"""
+HOW TO USE:
+COPY YOUR the3.py FILE TO THIS FOLDER
+"""
+
+grade = 0
+wrongs = []
+f = open("test_images")
+images = f.read().splitlines()
+f.close()
+
+f = open("test_patterns")
+patterns = f.read().splitlines()
+f.close()
+
+f = open("answers")
+answers = f.read().splitlines()
+f.close()
+
+l = 0
+tested_answers = []
+for image in images:
+    image = eval(image)
+    for i in patterns[l*20 : (l + 1)*20]:
+        i = eval(i)
+        a = pattern_search(i,image)
+        tested_answers.append(a)
+    l += 1
+
+for i in range(len(answers)):
+    if str(answers[i]) == str(tested_answers[i]):
+        grade += 1
+    
+    else:
+        wrongs.append([i, i//20, answers[i], tested_answers[i]])
+
+print(f"Grade: {grade}")
+if wrongs:
+    print("INDEX, IMAGE INDEX, TRUE ANSWER, YOUR ANSWER")
+    for i in wrongs:
+        print(i)
