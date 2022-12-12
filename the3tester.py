cd3eb790beb67bbc2c97ffc65c4293abc7706f5f
taylananas <shnitailan65@gmail.com>
2022-12-13 01:13:59 +0300

    31 lines

--- the3tester.py
+++ the3tester.py
@@ -1,24 +1,15 @@
-from the3 import pattern_search
-
-"""
-HOW TO USE:
-COPY YOUR the3.py FILE TO THIS FOLDER
-"""
-
+from the3 import pattern_search #COPY YOUR THE3.PY FILE INTO THIS FOLDER
 grade = 0
 wrongs = []
 f = open("test_images")
 images = f.read().splitlines()
 f.close()
-
 f = open("test_patterns")
 patterns = f.read().splitlines()
 f.close()
-
 f = open("answers")
 answers = f.read().splitlines()
 f.close()
-
 l = 0
 tested_answers = []
 for image in images:
@@ -28,14 +19,11 @@ for image in images:
         a = pattern_search(i,image)
         tested_answers.append(a)
     l += 1
-
 for i in range(len(answers)):
     if str(answers[i]) == str(tested_answers[i]):
         grade += 1
-    
     else:
         wrongs.append([i, i//20, answers[i], tested_answers[i]])
-
 print(f"Grade: {grade}")
 if wrongs:
     print("INDEX, IMAGE INDEX, TRUE ANSWER, YOUR ANSWER")
